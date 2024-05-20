383f178fec192139fab72ca54af2e9e1c3844184
“Guada” <guadalupearroferia@gmail.com>
2024-05-21 09:29:40 +1000

    ADD logic

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ import express from "express";
 
 const server = express();
 
+try {
+    server.use(express.static("public", {extensions: ["html"]}));
+
+    server.use("*",(req, res) =>{
+    res.status(404).send("Error 404")
+    })
+} catch (error) {
+    server.use("*",(req, res) =>{
+        res.status(500).send("Error 500")
+        })
+}
+
+
+server.listen(3000, ()=> console.log("Servidor corriendo correctamente"));
